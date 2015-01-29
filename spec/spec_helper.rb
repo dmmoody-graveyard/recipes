fd069304fc87284fa6ca2f4764c068be4929c1c6
Duane M. Moody <dmmoody@gmail.com>
2015-01-29 10:20:59 -0800

    add destroy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,11 @@ require('bundler/setup')
 Bundler.require(:default, :test)
 
 Dir[File.dirname(__FILE__) + '/../lib/*.rb'].each { |file| require file }
+
+RSpec.configure do |config|
+  config.after(:each) do
+    Recipe.all().each() do |recipe|
+      recipe.destroy()
+    end
+  end
+end
