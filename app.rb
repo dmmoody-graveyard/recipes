90ae174bcf644581c616876760fc3b7ab388b9a1
Duane M. Moody <dmmoody@gmail.com>
2015-01-29 10:54:14 -0800

    add new recipe

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
+ENV['RACK_ENV'] = 'test'
+
 require('bundler/setup')
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @recipes = Recipe.all()
+  erb(:index)
+end
+
+post('/recipes') do
+  name = params.fetch('name')
+  @recipe = Recipe.new({:name => name})
+  @recipe.save()
   @recipes = Recipe.all()
   erb(:index)
 end
