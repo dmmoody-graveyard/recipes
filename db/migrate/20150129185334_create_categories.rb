1059ee5e0a9da35570cf691d3bd22f2dc21f287c
Duane M. Moody <dmmoody@gmail.com>
2015-01-29 10:56:40 -0800

    add category migration table

--- db/migrate/20150129185334_create_categories.rb
+++ db/migrate/20150129185334_create_categories.rb
@@ -1,4 +1,8 @@
 class CreateCategories < ActiveRecord::Migration
   def change
+    create_table(:categories) do |t|
+      t.column(:name, :string)
+    end
+    add_index(:categories, :name)
   end
 end
